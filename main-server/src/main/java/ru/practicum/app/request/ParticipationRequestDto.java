a1a888e6cdc40ad1c1df750b86f15fe90885755d
Deonis <deneshua@gmail.com>
2022-10-07 20:50:17 +0300

    update: добавлена аннотация JSON PAttern

--- main-server/src/main/java/ru/practicum/app/request/ParticipationRequestDto.java
+++ main-server/src/main/java/ru/practicum/app/request/ParticipationRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.app.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class ParticipationRequestDto {
 
     Integer id;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime created;
 
     Integer event;
--- main-server/src/main/java/ru/practicum/app/request/RequestDto.java
+++ main-server/src/main/java/ru/practicum/app/request/RequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.app.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ public class RequestDto {
 
     private Event event;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
 
     private Integer requester;
