afe8398fe2d5a463ca6c8fcb746b01b58729b032
Deonis <deneshua@gmail.com>
2022-10-12 16:52:18 +0400

    update: изменен докер композ

--- main-server/src/main/java/ru/practicum/app/statistic/clients/EntryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-//package ru.practicum.app.statistic.clients;
-//
-//public class EntryNotFoundException extends Throwable {
-//    public EntryNotFoundException(String message) {
-//        super(message);
-//    }
-//}
