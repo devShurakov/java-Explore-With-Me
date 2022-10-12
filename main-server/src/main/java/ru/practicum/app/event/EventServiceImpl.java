fabe644d272fe47afccc714c6f05cc8fb72272c4
Deonis <deneshua@gmail.com>
2022-10-12 12:04:25 +0400

    fix: исправлен метод поиска по фильтрам

--- main-server/src/main/java/ru/practicum/app/event/EventServiceImpl.java
+++ main-server/src/main/java/ru/practicum/app/event/EventServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import ru.practicum.app.category.Category;
 import ru.practicum.app.category.CategoryRepository;
