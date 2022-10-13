ea4ef25310af9056ba5e3503a3739d4c0ef93f86
Deonis <deneshua@gmail.com>
2022-10-13 15:48:28 +0400

    update: log.trace

--- main-server/src/main/java/ru/practicum/app/exceptionHandler/ErrorHandler.java
+++ main-server/src/main/java/ru/practicum/app/exceptionHandler/ErrorHandler.java
@@ -3,7 +3,6 @@ package ru.practicum.app.exceptionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
